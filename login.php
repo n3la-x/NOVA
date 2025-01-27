2aa26daa182c1ebd6bfb501fc270b9e0eb8af7e8
AnesaaM <anesamecinaj28@gmail.com>
2025-01-27 12:28:50 +0100

    shtimi i php ne login

--- login.php
+++ login.php
@@ -8,13 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $connection = $db->getConnection();
     $user = new User($connection);
 
-    // Get form data
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Attempt to log in
+
     if ($user->login($email, $password)) {
-        header("Location: Nova.php"); // Redirect to home page
+        header("Location: Nova.php");
         exit;
     } else {
         echo "Invalid login credentials!";
