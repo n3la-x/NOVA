0f8d40ed9bed4f0d427d76c0c5bd783309e7298d
n3la-x <ornelaxoxa@gmail.com>
2025-01-30 14:43:49 +0100

    nryshime ne ngjyra ne sign in dhe shtimi i link per font dhe dizajn

--- Signin.php
+++ Signin.php
@@ -85,19 +85,38 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Parkinsans:wght@300..800&family=Tangerine:wght@400;700&display=swap" rel="stylesheet">
+<!--font link-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Charm:wght@400;700&family=Pangolin&family=Parkinsans:wght@300..800&family=Poiret+One&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Tangerine:wght@400;700&display=swap" rel="stylesheet">
+<!--font link end-->
     <title>Nova</title>
     <style>
         body {
             display: flex;
             justify-content: center;
             align-items: center;
-            background-image: url('images/intro.jpg'); /* Ensure the image path is correct */
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
             height: 100vh;
             margin: 0;
-            font-family: 'Ogg', sans-serif;
+            font-family:'Poiret One';
+            background-color: #e5e1dab7;
         }
 
         .signUpPart {
@@ -111,7 +130,7 @@ if (isset($_POST["submit"])) {
         }
 
         .signUpPart h1 {
-            color: #7f2549;
+            color:  rgb(168, 124, 124);
         }
 
         form {
@@ -124,7 +143,7 @@ if (isset($_POST["submit"])) {
             width: 250px;
             height: 50px;
             font-size: 14px;
-            border: 2.5px solid #7f2549;
+            border: 2.5px solid  rgb(168, 124, 124);
             border-radius: 5px;
             margin: 10px;
             padding: 0 10px;
@@ -134,7 +153,7 @@ if (isset($_POST["submit"])) {
             width: 120px;
             height: 40px;
             font-size: 14px;
-            background-color: #7f2549;
+            background-color: rgb(168, 124, 124);
             color: #fff;
             border: none;
             border-radius: 5px;
@@ -142,7 +161,7 @@ if (isset($_POST["submit"])) {
         }
 
         #signUp:hover {
-            background-color: #6a1c36;
+            background-color: rgb(202, 135, 135);
         }
 
         .alert {
