7d793e255dac913d5bd8882cd84a7f9fb631f3a8
AnesaaM <anesamecinaj28@gmail.com>
2025-02-01 00:40:24 +0100

    ndryshime te vogla

--- dashboard.php
+++ dashboard.php
@@ -490,15 +490,13 @@ footer {
 <div id="mySidebar" class="sidebar">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="bi bi-x-circle"></i></a>
   <a href="aboutus.html"><i class="bi bi-person-lines-fill"></i>About us</a>
-  <a href="login.html"><i class="bi bi-box-arrow-in-right"></i> Log in</a>
-  <a href="ContactUs.html"><i class="bi bi-telephone"></i>Contact us</a>
+  <a href="LogIn.php"><i class="bi bi-box-arrow-in-right"></i> Log in</a>
+  <a href="contactUs.php"><i class="bi bi-telephone"></i>Contact us</a>
   <a href="SpecialOffers.html"><i class="bi bi-stars"></i>Special offers</a>
-  <a href="Dashboard.html">Dashboard</a>
+ 
   
 
 </div>
-
-
 <!-- -------------------------Pop-up form--------------- -->
 <div id="popup" class="popup-overlay">
   <div class="popup-content">
