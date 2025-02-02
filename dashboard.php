1bf520ace55d37677b47b04ada7ece5543592c20
AnesaaM <anesamecinaj28@gmail.com>
2025-02-02 21:49:45 +0100

    databaza e contact us

--- dashboard.php
+++ dashboard.php
@@ -526,10 +526,11 @@ footer {
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="bi bi-x-circle"></i></a>
   <a href="aboutus.html"><i class="bi bi-person-lines-fill"></i>About us</a>
   <a href="LogIn.php"><i class="bi bi-box-arrow-in-right"></i> Log in</a>
-  <a href="ContactUs.html"><i class="bi bi-telephone"></i>Contact us</a>
+  <a href="ContactUs.html"><i class="bi bi-telephone"></i>Contact us </a>
   <a href="SpecialOffers.html"><i class="bi bi-stars"></i>Special offers</a>
   <a href="add_studio.php"><i class="bi bi-patch-plus-fill"></i>Add Studio</a>
   <a href="bookings.php"><i class="bi bi-patch-plus-fill"></i>Bookings</a>
+  <a href="contactUs.php"><i class="bi bi-patch-plus-fill"></i>Contact Us Messages</a>
   <div class="spacer"></div>
   <a class="logout-btn" href="logout.php"><i class="bi bi-box-arrow-in-left"></i>Logout</a>
   
